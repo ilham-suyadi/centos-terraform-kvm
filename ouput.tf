dbc151f84b5d19034a1b2581a336b32700a43727
ilham <72326498+ilham3113@users.noreply.github.com>
2023-02-13 09:00:56 +0700

    Update ouput.tf

--- ouput.tf
+++ ouput.tf
@@ -1,5 +1,5 @@
 output "centos_terrafrom_ipaddr" {
-   value = libvirt_domain.centos_terraform.*.network_interface.0.ad
+   value = libvirt_domain.centos_terraform.*.network_interface.0.addresses
 }
 
 # showing ip from VMs
